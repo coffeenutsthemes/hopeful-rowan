b2067be187e3ca05e0373900fc5e18b8d1683846
Prryns <coffeenutsthemes@users.noreply.github.com>
2018-01-24 02:19:52 +0800

    Update sc.js

--- javascripts/sc.js
+++ javascripts/sc.js
@@ -1,13 +1,10 @@
 // minimal soundcloud player © shythemes.tumblr
 $.fn.soundCloud = function( options ) {
-        // This is the easiest way to have default options.
         var settings = $.extend({
-            // These are the defaults.
-            playbtnColor: "#556b2f"
+            playColor: "#556b2f"
         }, options );
  
-        // Greenify the collection based on the settings variable.
-        var color = settings.playbtnColor;
+        var color = settings.playColor;
         return this.each(function(){
              $(this).attr({ src: $(this).attr('src').split('&')[0] + '&amp;visual=false&amp;liking=false&amp;sharing=false&amp;auto_play=false&amp;show_comments=false&amp;continuous_play=false&amp;origin=tumblr&amp;color=' + color.split('#')[1], maxheight: 20, maxwidth: 590 });
     };
